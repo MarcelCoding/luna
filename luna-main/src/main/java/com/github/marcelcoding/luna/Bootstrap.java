1380ed6c4022cfd3265ed05d6eaf2eb931837d78
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-02-27 17:33:03 +0100

    Updated docker build

--- luna-main/src/main/java/com/github/marcelcoding/luna/Bootstrap.java
+++ luna-main/src/main/java/com/github/marcelcoding/luna/Bootstrap.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@SuppressWarnings("checkstyle:HideUtilityClassConstructor")
 public class Bootstrap {
 
   public static void main(final String[] args) {
