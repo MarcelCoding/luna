e2b0c8ef31b41ee4d765fa79274c00932d91c724
Marcel <34819524+MarcelCoding@users.noreply.github.com>
2021-06-21 15:51:53 +0200

    Fixed repository scanning bug

--- luna-weather/src/main/java/com/github/marcelcoding/luna/weather/WeatherConfiguration.java
+++ luna-weather/src/main/java/com/github/marcelcoding/luna/weather/WeatherConfiguration.java
@@ -1,10 +1,13 @@
 package com.github.marcelcoding.luna.weather;
 
+import com.github.marcelcoding.luna.weather.repository.SensorGroupRepository;
 import org.springdoc.core.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @Configuration
+@EnableJpaRepositories(basePackageClasses = SensorGroupRepository.class)
 public class WeatherConfiguration {
 
   @Bean
